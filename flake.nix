84afbcca70827c22741c5b767cf9208aeabb5ef7
Thomas Schafer <thomasschafer97@gmail.com>
2025-06-11 21:12:04 +0100

    Add test workflow

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  description = "Scooter - Interactive find and replace TUI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+
+        testDeps = with pkgs; [
+          diffutils
+          expect
+          fd
+          nushell
+          ripgrep
+          rustToolchain
+          sd
+        ];
+
+        benchmarkDeps =
+          testDeps
+          ++ (with pkgs; [
+            hyperfine
+            rsync
+            findutils
+          ]);
+
+        shellDeps = with pkgs; [
+          rustToolchain
+          rust-analyzer
+          cargo-insta
+        ];
+      in
+      {
+        apps.end-to-end-test = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellScriptBin "run-end-to-end-test" ''
+            export PATH="${pkgs.lib.makeBinPath testDeps}:$PATH"
+            set -e
+            echo "Building..."
+            cargo build --release --locked
+            echo "Running end-to-end tests..."
+            nu tests/compare-tools.nu test "$@"
+          '';
+        };
+
+        apps.benchmark = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellScriptBin "run-benchmark" ''
+            export PATH="${pkgs.lib.makeBinPath benchmarkDeps}:$PATH"
+            set -e
+            echo "Building..."
+            cargo build --release --locked
+            echo "Running benchmarks..."
+            nu tests/compare-tools.nu benchmark "$@"
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = shellDeps;
+
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+
+        devShells.benchmark = pkgs.mkShell {
+          nativeBuildInputs = shellDeps ++ benchmarkDeps;
+
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+      }
+    );
+}
---
+++ tests/README.md
@@ -0,0 +1,36 @@
+# End-to-end tests
+
+This directory contains end-to-end tests and benchmarks for Scooter that compare its behavior against other find and replace tools.
+
+## Prerequisites
+
+### Installing Nix
+
+Install Nix - the Determinate Systems installer can be found [here](https://determinate.systems/nix-installer/), but other methods are available.
+
+## Running tests
+
+From the project root, run:
+
+```bash
+nix run .#end-to-end-test
+
+To run tests against the Linux kernel instead of test files:
+
+```sh
+nix run .#end-to-end-test -- --use-linux
+```
+
+## Benchmarks
+
+Benchmarks are run with [hyperfine](https://github.com/sharkdp/hyperfine). To run them:
+
+```sh
+nix run .#benchmark
+```
+
+To update the readme with the benchmark results:
+
+```sh
+nix run .#benchmark -- --update-readme
+```
